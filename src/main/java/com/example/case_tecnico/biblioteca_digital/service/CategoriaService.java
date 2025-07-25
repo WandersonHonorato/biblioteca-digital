d4c1f20cfbab7a40b23431c35235621982e8b5a3
Wanderson Honorato <wanderson.honorato1998@gmail.com>
2025-07-25 15:17:57 -0300

    Refactor: implementa a lógica de dto para entity

--- src/main/java/com/example/case_tecnico/biblioteca_digital/service/CategoriaService.java
+++ src/main/java/com/example/case_tecnico/biblioteca_digital/service/CategoriaService.java
@@ -1,4 +1,44 @@
 package com.example.case_tecnico.biblioteca_digital.service;
 
-public class CategoriaService {
+import com.example.case_tecnico.biblioteca_digital.dto.CategoriaDTO;
+import com.example.case_tecnico.biblioteca_digital.model.Categoria;
+import com.example.case_tecnico.biblioteca_digital.repository.CategoriaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public static class CategoriaService {
+    private final CategoriaRepository categoriaRepository;
+
+    public List<CategoriaDTO> listarTodas() {
+        return categoriaRepository.findAll().stream().map(this::toDTO).collect(Collectors.toList());
+    }
+
+    public CategoriaDTO criar(CategoriaDTO dto) {
+        Categoria categoria = Categoria.builder()
+                .nome(dto.nome())
+                .descricao(dto.descricao())
+                .build();
+        return toDTO(categoriaRepository.save(categoria));
+    }
+
+    public CategoriaDTO buscarPorId(Long id) {
+        Categoria categoria = categoriaRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Categoria não encontrada"));
+        return toDTO(categoria);
+    }
+
+    private CategoriaDTO toDTO(Categoria categoria) {
+        return new CategoriaDTO(
+                categoria.getId(),
+                categoria.getNome(),
+                categoria.getDescricao()
+        );
+    }
 }
+
