bb93c1598db52d361e7d4b2c3659e7a5525852af
Wanderson Honorato <wanderson.honorato1998@gmail.com>
2025-07-25 16:11:04 -0300

    Refactor: implementa as Bean Validation JPA

--- src/main/java/com/example/case_tecnico/biblioteca_digital/dto/CategoriaDTO.java
+++ src/main/java/com/example/case_tecnico/biblioteca_digital/dto/CategoriaDTO.java
@@ -1,7 +1,12 @@
 package com.example.case_tecnico.biblioteca_digital.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CategoriaDTO(
         Long id,
+
+        @NotBlank(message = "Nome da categoria é obrigatório")
         String nome,
+
         String descricao
 ) {}
